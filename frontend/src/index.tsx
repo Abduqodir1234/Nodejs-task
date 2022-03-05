db92d108dfdf4973f30ceeb84e2ad16f0f26b18f
Abduqodir1234 <gofurboyevabduqodir@gmail.com>
2022-03-05 16:39:58 +0500

    removing quill css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css'; 
 import "./public/statics/index.css"
-import 'quill/dist/quill.snow.css';
 import reportWebVitals from './reportWebVitals';
 import {QueryClient,QueryClientProvider} from 'react-query'
 import {ReactQueryDevtools} from 'react-query/devtools'
