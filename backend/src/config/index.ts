d70969cb41bd751abd647222b31734d3aa8e378a
Abduqodir1234 <gofurboyevabduqodir@gmail.com>
2022-03-05 13:42:35 +0500

    creating env

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,10 +2,6 @@ export const configs = {
     //Db Config
 
     dbconfig:{
-        db_name:"test1",
-        db_user:"root",
-        db_pass:"1242",
-        host:'localhost',
         pool:{
             max:5,
             min:0,
@@ -16,13 +12,6 @@ export const configs = {
 
 
     //JWT configs
-
-    jwtconfigs:{
-        token_secret:'alskfd23asdf4lasdf+kjwl6456543346weqr6834q2r+/**/-589+985+6985wek',
-        access_validity:'1m',
-        refresh_validity:'30d'
-    },
-
     blogsConfig:{
         limit:10
     }
